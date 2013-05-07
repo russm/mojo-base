8da5ff58af461d82dc6b08195125e04d7ca1303b
russm <russm@slofith.org>
2013-05-07 12:18:55 +1000

    explicitly use wire connections in serial_loop

--- serial_loop.v
+++ serial_loop.v
@@ -1,8 +1,8 @@
 module serial_loop (
-  input [7:0] rx_data,
-  input new_rx_data,
-  output [7:0] tx_data,
-  output new_tx_data
+  input wire [7:0] rx_data,
+  input wire new_rx_data,
+  output wire [7:0] tx_data,
+  output wire new_tx_data
 );
 
 assign tx_data = rx_data;
